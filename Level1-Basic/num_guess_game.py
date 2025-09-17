9ad252adb35645d1827efd59559a46cee4e2b884
Shubham <shubhu2961@gmail.com>
2025-09-17 12:34:52 +0545

    edited number guess game code

--- Level1-Basic/num_guess_game.py
+++ Level1-Basic/num_guess_game.py
@@ -4,9 +4,9 @@ number = random.randint(1, 100)
 
 print("Welcome to Number Guessing Game!")
 print("I have picked a number between 1 and 100.")
-print("You have 7 chances to guess it correctly.")
+print("You have 10 chances to guess it correctly.")
 
-max_attempts = 7
+max_attempts = 10
 attempts = 0
 
 while attempts < max_attempts:
